121907db3510a1a87161a5b93dd8c9cf4313d45f
Daniel Fasiku <adewolefas@gmail.com>
2024-09-17 05:41:19 +0100

    Homepage styling and basic functionality

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import HeroSection from "./Components/HeroSection/HeroSection";
+import NavBar from "./Components/NavBar/NavBar";
+import './App.css'
+
+const App = () => {
+  return (
+    <div>
+      <NavBar />
+      <HeroSection />
+    </div>
+  )
+}
+
+export default App
