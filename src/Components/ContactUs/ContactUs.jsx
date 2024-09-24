c3fe9e459c5f8b39d569e9fdc5ff920074a54c98
Daniel Fasiku <adewolefas@gmail.com>
2024-09-24 01:49:03 +0100

    activated message form

--- src/Components/ContactUs/ContactUs.jsx
+++ src/Components/ContactUs/ContactUs.jsx
@@ -1,23 +1,40 @@
 import { Icon } from "@iconify/react";
 import PropTypes from "prop-types";
-import {useState} from 'react'
+import { useState, useRef } from 'react';
+import emailjs from '@emailjs/browser';
 
 import ContactUsImage from "../../assets/contact-us.png";
 import "./ContactUs.css"
 
-const ContactUs = ({id = ""}) => {
-    const [form, setForm] = useState({name: "", email: "", message: ""});
+const ContactUs = ({ id = "" }) => {
+    const [form, setForm] = useState({ name: "", email: "", message: "" });
+    const sendForm = useRef();
 
     const handleFormInput = (event) => {
-        const {id, value} = event.target;
+        const { id, value } = event.target;
         setForm((prev) => ({
             ...prev,
             [id]: value
         }))
     };
 
-    const submitForm = (event) => {
+    const sendEmail = (event) => {
         event.preventDefault();
+        emailjs
+            .sendForm('service_bj0wq37', 'template_fk7bz3p', sendForm.current, {
+                publicKey: 'CHsfcMwRuAQxJF0Vq',
+            })
+            .then(
+                () => {
+                    alert("Message Sent")
+                    sendForm.current.reset
+                    console.log('SUCCESS!');
+                },
+                (error) => {
+                    alert("Message failed, please try again")
+                    console.log('FAILED...', error.text);
+                },
+            );
     };
 
     return (
@@ -25,22 +42,22 @@ const ContactUs = ({id = ""}) => {
             <div className="cu-left-section">
                 <h1 className="cu-text1">Contact Us</h1>
                 <p className="cu-text2">we are here to work with you! feel free to contact us, whether you have questions, feedback, inquiries or want to work with us, our team is ready to help.</p>
-                <form onSubmit={submitForm} className="cu-form">
+                <form ref={sendForm} onSubmit={sendEmail} className="cu-form">
                     <label htmlFor="name" className="cu-form-label" >Name</label>
                     <div className="cu-input-wrapper">
                         <Icon icon="iconamoon:profile" width={30} height={30} />
-                        <input type="text" id='name' value={form.name} onChange={handleFormInput} className="cu-input" placeholder="Enter Your Name" />
+                        <input type="text" id='name' name="user_name" value={form.name} onChange={handleFormInput} className="cu-input" placeholder="Enter Your Name" />
                     </div>
                     <label htmlFor="e-mail" className="cu-form-label">E-mail</label>
                     <div className="cu-input-wrapper">
                         <Icon icon="iconoir:mail" width={30} height={30} />
-                        <input type="text" id='email' value={form.email} onChange={handleFormInput} className="cu-input" placeholder="Enter Your E-mail" />
+                        <input type="text" id='email' name="user_email" value={form.email} onChange={handleFormInput} className="cu-input" placeholder="Enter Your E-mail" />
                     </div>
                     <div className="cu-message-wrapper" >
                         <label htmlFor="message" className="cu-form-label">Message</label>
-                        <textarea type="text" id='message' value={form.message} onChange={handleFormInput} className="cu-input2" placeholder="Write Your Message" />
+                        <textarea type="text" id='message' name="message" value={form.message} onChange={handleFormInput} className="cu-input2" placeholder="Write Your Message" />
                     </div>
-                    <button className="cu-form-button">
+                    <button type="submit" value="Send" className="cu-form-button">
                         <p>Send Message</p>
                         <Icon icon="iconamoon:arrow-right-1" className="cu-form-button-icon" color="#ffffff" />
                     </button>
